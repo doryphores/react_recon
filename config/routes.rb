fe29cae520c98e59096735b5df872e6645955226
Pawel Dawczak <pawel.dawczak@gmail.com>
2015-11-13 19:38:38 +0000

    Jasmine test

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
   root to: "imports#index"
 
   resources :imports do
---
+++ spec/javascripts/foo_spec.js
@@ -0,0 +1,7 @@
+// require helpers/some_helper_file
+
+describe("Testing!", function () {
+  it("does something", function () {
+    expect(1 + 1).toBe(2);
+  });
+});
